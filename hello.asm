f39613215abd43cfe05b11740c3ab1877903d2f1
alvaro <alvaro10048@protonmail.com>
2023-01-28 03:13:34 -0300

    1

---
+++ hello.asm
@@ -0,0 +1,18 @@
+section .data
+    msg db 'Meu Primeiro Progrma em Assembly!', 0xA
+    tam equ $- msg
+
+section .text
+
+global _start
+
+_start:
+  mov EAX, 0x4
+  mov EBX, 0x1
+  mov ECX, msg
+  mov EDX, tam
+  int 0x80
+
+  mov EAX, 0x1 ; finaliza
+  mov EBX, 0x0 ; return 0
+  int 0x80
